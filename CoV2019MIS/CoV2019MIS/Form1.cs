77ee7006cdf2f69c550ae581136a6bad41eef923
LPJworkroom <lpjworkroom@gmail.com>
2020-02-20 14:31:30 +0800

    fff

--- CoV2019MIS/CoV2019MIS/Form1.cs
+++ CoV2019MIS/CoV2019MIS/Form1.cs
@@ -26,14 +26,14 @@ namespace CoV2019MIS
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            this.overallInfo = new Overallinfo(new Label[]{(Label)Controls.Find("TodayConfirmed",false)[0],
+            /*this.overallInfo = new Overallinfo(new Label[]{(Label)Controls.Find("TodayConfirmed",false)[0],
                                                 (Label)Controls.Find("TodaySuspect",false)[0],
                                                 (Label)Controls.Find("TodayDeath",false)[0]},
                                                new Label[]{(Label)Controls.Find("SumConfirmed",false)[0],
                                                 (Label)Controls.Find("SumSuspect",false)[0],
                                                 (Label)Controls.Find("SumDeath",false)[0],},
                                                 new int[] {1,2,3},
-                                                new int[] { 4, 5, 6 });
+                                                new int[] { 4, 5, 6 });*/
         }
 
         private void label1_Click(object sender, EventArgs e)
